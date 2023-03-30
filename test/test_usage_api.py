67d72ba32552f3b8b58fb4a20c72b1f8862f0be7
Santhosh K <santhoshk@unicourt.com>
2023-03-30 19:46:25 +0530

    updated billing cycle

--- test/test_usage_api.py
+++ test/test_usage_api.py
@@ -7,7 +7,7 @@ class TestUsageAPI:
 
     def test_get_billing_usage_by_billing_cycle():
         return Usage.get_billing_usage_by_billing_cycle(
-            billing_cycle="2023-02-20to2023-03-20")
+            billing_cycle="2023-01-25to2023-02-25")
 
     def test_get_daily_usage_by_date():
         return Usage.get_daily_usage_by_date(date='2023-02-21')
