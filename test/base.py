5ab738f9331ec4e2877945ae656da6c2091db777
Santhosh K <santhoshk@unicourt.com>
2023-03-30 16:25:36 +0530

    Update base.py

--- test/base.py
+++ test/base.py
@@ -45,7 +45,7 @@ class TestBase:
                     try:
                         _, status_code = function()
                         print(method_name, status_code)
-                        time.sleep(1)
+                        # time.sleep(1)
                         #instance_obj.log(method_name, status_code)
                     except Exception as e:
                         #instance_obj.log(method_name, status_code)
