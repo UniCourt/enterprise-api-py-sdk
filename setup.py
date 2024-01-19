f31f687dc1a1389ca93609c9a9038f68d4313fe7
Snehal Sakhardande <snehals@unicourt.com>
2024-01-19 06:25:52 +0000

    Initial merge

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "openapi-client"
-VERSION = "1.0.0"
+NAME = "unicourt"
+VERSION = "1.0"
 # To install the library, run the following
 #
 # python setup.py install
---
+++ unicourt/utils.py
@@ -0,0 +1,13 @@
+import unicourt
+from collections import namedtuple
+
+# access client config through this function.
+
+
+def api_client():
+    if not unicourt.configuration:
+        unicourt.configuration = unicourt.Configuration(
+            access_token=unicourt.ACCESS_TOKEN)
+    if not unicourt.api_client:
+        unicourt.api_client = unicourt.ApiClient(unicourt.configuration)
+    return unicourt.api_client
