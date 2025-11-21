ebc5df8edb96f819e770d1b985ef322b059f1aff
Pradeep Singh <pradeeps@unicourt.com>
2025-11-21 09:47:12 +0530

    Updated the test data with data returning status code 200

--- test/test_law_firm_analytics_api.py
+++ test/test_law_firm_analytics_api.py
@@ -4,28 +4,28 @@ from unicourt.sdk.LawFirmAnalytics import LawFirmAnalytics
 class TestLawFirmAnalytics:
     def test_get_norm_attorneys_associated_with_norm_law_firm():
         return LawFirmAnalytics.get_norm_attorneys_associated_with_norm_law_firm(
-            norm_law_firm_id='NORGrPmQyLdx9NGHcT',
+            norm_law_firm_id='NORGVHdWCDN7D6Ayn3',
             page_number=1,
-            q='caseTypeId:"CTYPATMYyaJekdgj2c" AND caseFiledDate:[2017-01-01T00:00:00+00:00TO2021-11-30T00:00:00+00:00]'
+            q='caseTypeId:"CTYPNjbKTN7Yfo2wdb" AND caseFiledDate:[2017-01-01T00:00:00+00:00 TO 2025-11-30T00:00:00+00:00]'
         )
 
     def test_get_norm_judges_associated_with_norm_law_firm():
         return LawFirmAnalytics.get_norm_judges_associated_with_norm_law_firm(
-            norm_law_firm_id='NORGrPmQyLdx9NGHcT',
+            norm_law_firm_id='NORGVHdWCDN7D6Ayn3',
             page_number=1,
-            q='caseTypeId:"CTYPATMYyaJekdgj2c" AND caseFiledDate:[2017-01-01T00:00:00+00:00TO2021-11-30T00:00:00+00:00]'
+            q='caseTypeId:"CTYPNjbKTN7Yfo2wdb" AND caseFiledDate:[2017-01-01T00:00:00+00:00 TO 2025-11-30T00:00:00+00:00]'
         )
 
     def test_get_norm_law_firm_by_id():
         return LawFirmAnalytics.get_norm_law_firm_by_id(
-            norm_law_firm_id='NORGDiJQPjeed2mtvx'
+            norm_law_firm_id='NORGVHdWCDN7D6Ayn3'
         )
 
     def test_get_norm_parties_associated_with_norm_law_firm():
         return LawFirmAnalytics.get_norm_parties_associated_with_norm_law_firm(
-            norm_law_firm_id='NORGrPmQyLdx9NGHcT',
+            norm_law_firm_id='NORGVHdWCDN7D6Ayn3',
             page_number=1,
-            q='caseTypeId:"CTYPATMYyaJekdgj2c" AND caseFiledDate:[2017-01-01T00:00:00+00:00TO2021-11-30T00:00:00+00:00]'
+            q='caseTypeId:"CTYPNjbKTN7Yfo2wdb" AND caseFiledDate:[2017-01-01T00:00:00+00:00 TO 2025-11-30T00:00:00+00:00]'
         )
 
     def test_search_normalized_law_firms():
--- test/test_party_analytics_api.py
+++ test/test_party_analytics_api.py
@@ -4,28 +4,28 @@ from unicourt.sdk.PartyAnalytics import PartyAnalytics
 class TestPartyAnalytics:
     def test_get_norm_attorneys_associated_with_norm_party():
         return PartyAnalytics.get_norm_attorneys_associated_with_norm_party(
-            q='caseTypeId:"CTYPATMYyaJekdgj2c" AND caseFiledDate:[2017-01-01T00:00:00+00:00TO2021-11-30T00:00:00+00:00]',
-            norm_party_id='NORGrPmQyLdx9NGHcT',
+            q='caseTypeId:"CTYPMrcrvoUeZHmsgk" AND caseFiledDate:[2017-01-01T00:00:00+00:00 TO 2025-11-30T00:00:00+00:00]',
+            norm_party_id='NORGVppBo6fB829yV9',
             page_number=1
         )
 
     def test_get_norm_judges_associated_with_norm_party():
         return PartyAnalytics.get_norm_judges_associated_with_norm_party(
-            norm_party_id='NORGrPmQyLdx9NGHcT',
+            norm_party_id='NORGVppBo6fB829yV9',
             page_number=1,
-            q='caseTypeId:"CTYPATMYyaJekdgj2c" AND caseFiledDate:[2017-01-01T00:00:00+00:00TO2021-11-30T00:00:00+00:00]'
+            q='caseTypeId:"CTYPMrcrvoUeZHmsgk" AND caseFiledDate:[2017-01-01T00:00:00+00:00 TO 2025-11-30T00:00:00+00:00]',
         )
 
     def test_get_norm_law_firms_associated_with_norm_party():
         return PartyAnalytics.get_norm_law_firms_associated_with_norm_party(
-            norm_party_id='NORGrPmQyLdx9NGHcT',
+            norm_party_id='NORGVppBo6fB829yV9',
             page_number=1,
-            q='caseTypeId:"CTYPATMYyaJekdgj2c" AND caseFiledDate:[2017-01-01T00:00:00+00:00TO2021-11-30T00:00:00+00:00]'
+            q='caseTypeId:"CTYPMrcrvoUeZHmsgk" AND caseFiledDate:[2017-01-01T00:00:00+00:00 TO 2025-11-30T00:00:00+00:00]',
         )
 
     def test_get_norm_party_by_id():
         return PartyAnalytics.get_norm_party_by_id(
-            norm_party_id='NORGrPmQyLdx9NGHcT'
+            norm_party_id='NORGVppBo6fB829yV9'
         )
 
     def test_search_normalized_parties():
