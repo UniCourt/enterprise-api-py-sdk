49bdb96cb52f7ec8984bd1198c5f45370dcce5c5
Pradeep Singh <pradeeps@unicourt.com>
2025-11-21 10:21:13 +0530

    Bumped unicourt SDK patch version in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def _generate_description():
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "unicourt"
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
